492d8cf9ddf06ddffdde7da8d35b149ecf9eb962
hirotonomi <asztonpsiak@gmail.com>
2024-04-15 10:22:45 +0200

    navbar and second route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'games', loadChildren: () => import('./games/games.module').then(m => m.GamesModule) }];
+const routes: Routes = [{ path: 'games', loadChildren: () => import('./games/games.module').then(m => m.GamesModule) }, { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
